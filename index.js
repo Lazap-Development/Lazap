92c764f1329f0132feeb2ce39a1cdf91ac712ed0
DashCruft <theporyafm12@gmail.com>
2021-11-17 19:31:08 -0800

    Library now has its own preview

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = electron.app;
 require('v8-compile-cache');
 
 app.on('ready', () => {
-    const mainWindow = new electron.BrowserWindow({width: 1100, height: 600, resizable: false, transparent: true });
+    const mainWindow = new electron.BrowserWindow({width: 1100, height: 600, resizable: true, transparent: true });
 
     mainWindow.loadFile('src/index.html')
 });
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,17 +1,28 @@
+// Library
+document.getElementById("library").addEventListener("mouseover", function (event) {
+    document.getElementById("backgroundId").style.backgroundImage = "";
+    document.getElementById("backgroundId").style.backgroundColor = "#141414";
+    document.getElementById("libraytext").innerHTML = `<i class="bi bi-collection-fill" style="font-size: 80px"></i>
+    <br/>Your Personal Games library`;
+});
 
+        
 // Steam
 document.getElementById("steam").addEventListener("mouseenter", function (event) {
-    document.getElementById("backgroundId").style.backgroundImage = "url(../img/steam-preview.jpg) ";
+    document.getElementById("backgroundId").style.backgroundImage = "url(../img/steam-preview.jpg)";
+    document.getElementById("libraytext").innerHTML = ``;
 });
 
 // Epic Games
 document.getElementById("epic").addEventListener("mouseover", function (event) {
     document.getElementById("backgroundId").style.backgroundImage = "url(../img/epic-preview.jpg)";
+    document.getElementById("libraytext").innerHTML = ``;
 });
 
 // Rockstar Launcher
 document.getElementById("rockstar").addEventListener("mouseenter", function (event) {
     document.getElementById("backgroundId").style.backgroundImage = "url(../img/rockstar-preview.jpg)";
+    document.getElementById("libraytext").innerHTML = ``;
 });
 
 document.getElementById("backgroundId").innerHTML = `${window.navigator.platform}`;
