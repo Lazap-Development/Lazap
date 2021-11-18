4f0e4cd76ee8a476c210383c618b76258e31904c
DashCruft <theporyafm12@gmail.com>
2021-11-17 17:23:04 -0800

    Epic Games Image load fix

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -5,7 +5,7 @@ document.getElementById("steam").addEventListener("mouseenter", function (event)
 });
 
 // Epic Games
-document.getElementById("epic").addEventListener("mouseenter", function (event) {
+document.getElementById("epic").addEventListener("mouseover", function (event) {
     document.getElementById("backgroundId").style.backgroundImage = "url(../img/epic-preview.jpg)";
 });
 
