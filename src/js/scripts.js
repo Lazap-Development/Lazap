e32ab71eb0142e73dbf4521d560fa9a0d7ffc20b
DashCruft <theporyafm12@gmail.com>
2021-11-17 22:33:47 -0800

    Dock re-position & No more launcher preview on icon hover

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,34 +1,35 @@
 //Webpage On Load Event
 window.onload = (event) => {
     document.getElementById("libraytext").innerHTML = `<i class="bi bi-collection-fill" style="font-size: 80px"></i>
-    <br/>Your Personal Games library`;
+    <br/>Welcome to Lazap!`;
 }
+
 
 // Library
 document.getElementById("library").addEventListener("mouseover", function (event) {
     document.getElementById("backgroundId").style.backgroundImage = "";
     document.getElementById("backgroundId").style.backgroundColor = "#141414";
     document.getElementById("libraytext").innerHTML = `<i class="bi bi-collection-fill" style="font-size: 80px"></i>
-    <br/>Your Personal Games library`;
+    <br/>Personal Games library`;
 });
 
 
 // Steam
 document.getElementById("steam").addEventListener("mouseenter", function (event) {
-    document.getElementById("backgroundId").style.backgroundImage = "url(../img/steam-preview.jpg)";
-    document.getElementById("libraytext").innerHTML = ``;
+    document.getElementById("libraytext").innerHTML = `<i class="bi bi-collection-fill" style="font-size: 80px"></i>
+    <br/>Steam`;
 });
 
 // Epic Games
 document.getElementById("epic").addEventListener("mouseover", function (event) {
-    document.getElementById("backgroundId").style.backgroundImage = "url(../img/epic-preview.jpg)";
-    document.getElementById("libraytext").innerHTML = ``;
+    document.getElementById("libraytext").innerHTML = `<i class="bi bi-collection-fill" style="font-size: 80px"></i>
+    <br/>Epic Games`;
 });
 
 // Rockstar Launcher
 document.getElementById("rockstar").addEventListener("mouseenter", function (event) {
-    document.getElementById("backgroundId").style.backgroundImage = "url(../img/rockstar-preview.jpg)";
-    document.getElementById("libraytext").innerHTML = ``;
+    document.getElementById("libraytext").innerHTML = `<i class="bi bi-collection-fill" style="font-size: 80px"></i>
+    <br/>Rockstar Games Launcher`;
 });
 
 document.getElementById("backgroundId").innerHTML = `${window.navigator.platform}`;
