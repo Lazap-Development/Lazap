cc2f8c08a1cfe89b0fe117df87f392f2bbbb79cc
DashCruft <theporyafm12@gmail.com>
2021-11-17 16:56:17 -0800

    v8-compile-cache to improve electron load

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 
 const electron = require('electron');
 const app = electron.app;
+require('v8-compile-cache');
 
 app.on('ready', () => {
-    const mainWindow = new electron.BrowserWindow({width: 1100, height: 600, resizable: false,});
+    const mainWindow = new electron.BrowserWindow({width: 1100, height: 600, resizable: false, transparent: true });
 
     mainWindow.loadFile('src/index.html')
 });
