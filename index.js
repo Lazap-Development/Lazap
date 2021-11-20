0e2fd32b8a6a4d5467f482630a066301baac97f3
DashCruft <theporyafm12@gmail.com>
2021-11-19 19:50:13 -0800

    Massive UI update and Clensenes

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
 require('v8-compile-cache');
 const electron = require('electron');
 const app = electron.app;
-const delay = "3000"
+const delay = "3020"
+//const delay = "0"
 app.commandLine.appendSwitch('auto-detect', 'false');
 app.commandLine.appendSwitch('no-proxy-server')
 
 app.on('ready', () => {
     const mainWindow = new electron.BrowserWindow({
-        width: 950,
-        height: 500,
+        width: 1000,
+        height: 510,
         minWidth: 850,
         minHeight: 450,
         resizable: true,
         frame: false,
         show: false,
         webPreferences: {
-            devTools: false
-        },
-        vibrancy: "dark"
+            nodeIntegration: true,
+            devTools: true,
+            contextIsolation: false,
+        }
     });
     mainWindow.webContents.loadFile('src/splash.html')
 
--- src/js/rockstar-games.js
+++ src/js/rockstar-games.js
@@ -1,5 +1,41 @@
-const si = require('systeminformation');
+function doitrockstar() {
+    const si = require('systeminformation');
+    var exec = require('child_process').exec;
 
-si.osInfo()
-  .then(data => console.log(data.platform))
-  .catch(error => console.error(error));
+    si.osInfo()
+        .then(data => {
+            if (data.platform === "linux") {
+                const checkIfInstalled = exec("find . -name Launcher.exe | head -n 1")
+                //Check if Rockstar Games is already installed
+                if (checkIfInstalled) {
+                    const {
+                        VTexec
+                    } = require('open-term')
+
+                    VTexec(`"${checkIfInstalled}"`)
+                } else {
+                    const findWine = exec('which wine')
+
+                    if (!findWine == '/usr/bin/wine') {
+                        alert('Since you are on linux, You need to have winehq installed. Please refer to your Distro package manager and install it.');
+                        return;
+                    } else {
+                        alert("The setup for Rockstar Games is going to start now. \n\nPlease make sure you remember (or manualy set) the path you want to install rockstar games in. \n\nHit Ok to contniue once you are ready")
+
+                        const {
+                            VTexec
+                        } = require('open-term')
+                        VTexec(`(cd ~ && curl -O https://gamedownloads.rockstargames.com/public/installer/Rockstar-Games-Launcher.exe && wine $HOME/Rockstar-Games-Launcher.exe)`, {
+                            linux: {
+                                priorityTerms: ['xterm', 'guake', 'konsole', 'xfce'],
+                                terms: ['xterm', 'guake', 'konsole', 'xfce'],
+                            }
+                        })
+                    }
+                }
+
+            } else if (data.platform === "Windows") {
+                console.log("You are on windows!")
+            }
+        })
+}
