16cb960b86e934f86130e1640c19739e1df685e6
DashCruft <theporyafm12@gmail.com>
2021-11-21 19:08:10 -0800

    Reverted Content-Security-Policy meta tag since it prevents changing pfp

--- src/js/functions.js
+++ src/js/functions.js
@@ -1,17 +1,3 @@
-const { ipcRenderer } = require("electron");
-
-function close_window () {
-    ipcRenderer.send("close-window")
-}
-
-function max_window () {
-    ipcRenderer.send("max-window")
-}
-
-function min_window () {
-    ipcRenderer.send("min-window")
-}
-
 function loadFile(event) {
     var image = document.getElementById("output");
     image.src = URL.createObjectURL(event.target.files[0]);
