8e46084588eabc996ab154e4f3ec0da5c6f3b163
DashCruft <theporyafm12@gmail.com>
2021-11-24 18:18:26 -0800

    HUGE UPDATE! FINALLY!

--- src/js/listeners.js
+++ src/js/listeners.js
@@ -1,14 +1,19 @@
 const marker = document.querySelector("#indicator");
 const items = document.querySelectorAll(".side-tab");
 
-items.forEach(link => link.addEventListener("click",
-    e => indicator(e.target)));
+items.forEach(link => link.addEventListener("click", e => {
+    marker.style.top = "220px";
+    marker.style.height = "0px";
+    indicator(e.target)
+}));
 
 function indicator(item) {
     marker.style.top = item.offsetTop + "px";
     marker.style.height = "30px";
 }
 
-window.onkeydown = function(evt) {
-    if ((evt.code == "Minus" || evt.code == "Equal") && (evt.ctrlKey || evt.metaKey)) {evt.preventDefault()}
+window.onkeydown = function (evt) {
+    if ((evt.code == "Minus" || evt.code == "Equal") && (evt.ctrlKey || evt.metaKey)) {
+        evt.preventDefault()
+    }
 }
