d7929176c48b69c236f1cc34f3cb352b623ec5f0
DashCruft <theporyafm12@gmail.com>
2021-11-25 16:55:45 -0800

    Tab Click Support: Recently, All Games, Favs

--- src/js/listeners.js
+++ src/js/listeners.js
@@ -1,6 +1,37 @@
 const marker = document.querySelector("#indicator");
 const items = document.querySelectorAll(".side-tab");
+const home = document.getElementById("home");
+const recent = document.getElementById("recent");
+const games = document.getElementById("games");
+const favs = document.getElementById("favs");
 
+document.getElementById("home-btn").addEventListener("click", function () {
+    home.style.display = "flex";
+    recent.style.display = "none";
+    games.style.display = "none";
+    favs.style.display = "none";
+})
+
+document.getElementById("recent-btn").addEventListener("click", function () {
+    home.style.display = "none";
+    recent.style.display = "flex";
+    games.style.display = "none";
+    favs.style.display = "none";
+})
+
+document.getElementById("games-btn").addEventListener("click", function () {
+    home.style.display = "none";
+    recent.style.display = "none";
+    games.style.display = "flex";
+    favs.style.display = "none";
+})
+
+document.getElementById("favs-btn").addEventListener("click", function () {
+    home.style.display = "none";
+    recent.style.display = "none";
+    games.style.display = "none";
+    favs.style.display = "flex";
+})
 
 
 items.forEach(link => link.addEventListener("click", e => {
