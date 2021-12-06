d56a9af4f15acc6128e0706ef52e5d28f61467fc
Porya D (2377400) <2377400@newwestschools.ca>
2021-12-06 12:46:19 -0800

    Every side-tab works now

--- src/js/listeners.js
+++ src/js/listeners.js
@@ -4,12 +4,18 @@ const home = document.getElementById("home");
 const recent = document.getElementById("recent");
 const games = document.getElementById("games");
 const favs = document.getElementById("favs");
+const friends = document.getElementById("friends");
+const messages = document.getElementById("messages");
+const activity = document.getElementById("activity");
 
 document.getElementById("home-btn").addEventListener("click", function () {
     home.style.display = "flex";
     recent.style.display = "none";
     games.style.display = "none";
     favs.style.display = "none";
+    friends.style.display = "none";
+    messages.style.display = "none";
+    activity.style.display = "none";
 })
 
 document.getElementById("recent-btn").addEventListener("click", function () {
@@ -17,6 +23,9 @@ document.getElementById("recent-btn").addEventListener("click", function () {
     recent.style.display = "flex";
     games.style.display = "none";
     favs.style.display = "none";
+    friends.style.display = "none";
+    messages.style.display = "none";
+    activity.style.display = "none";
 })
 
 document.getElementById("games-btn").addEventListener("click", function () {
@@ -24,6 +33,9 @@ document.getElementById("games-btn").addEventListener("click", function () {
     recent.style.display = "none";
     games.style.display = "flex";
     favs.style.display = "none";
+    friends.style.display = "none";
+    messages.style.display = "none";
+    activity.style.display = "none";
 
     document.querySelector('div#games')
     require('./js/launchers/find-games.js').loadGames();
@@ -34,8 +46,40 @@ document.getElementById("favs-btn").addEventListener("click", function () {
     recent.style.display = "none";
     games.style.display = "none";
     favs.style.display = "flex";
+    messages.style.display = "none";
+    activity.style.display = "none";
+    friends.style.display = "none";
 })
 
+document.getElementById("messages-btn").addEventListener("click", function () {
+    home.style.display = "none";
+    recent.style.display = "none";
+    games.style.display = "none";
+    favs.style.display = "none";
+    messages.style.display = "flex";
+    activity.style.display = "none";
+    friends.style.display = "none";
+})
+
+document.getElementById("activity-btn").addEventListener("click", function () {
+    home.style.display = "none";
+    recent.style.display = "none";
+    games.style.display = "none";
+    favs.style.display = "none";
+    messages.style.display = "none";
+    activity.style.display = "flex";
+    friends.style.display = "none";
+})
+
+document.getElementById("friends-btn").addEventListener("click", function () {
+    home.style.display = "none";
+    recent.style.display = "none";
+    games.style.display = "none";
+    favs.style.display = "none";
+    messages.style.display = "none";
+    activity.style.display = "none";
+    friends.style.display = "flex";
+})
 
 items.forEach(link => link.addEventListener("click", e => {
     marker.style.top = "220px";
