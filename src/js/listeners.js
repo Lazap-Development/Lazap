035b71a4e1730c71114e8f74e1421cec0e7c5952
Gustavo Rocha <gugu.r.azevedo@gmail.com>
2022-01-10 19:48:06 -0300

    Add timeout to prevent visual bugs

--- src/js/listeners.js
+++ src/js/listeners.js
@@ -1,105 +1,110 @@
-const marker = document.getElementById("indicator");
-const items = document.querySelectorAll(".side-tab");
-const home = document.getElementById("home");
-const recent = document.getElementById("recent");
-const games = document.getElementById("games");
-const favs = document.getElementById("favs");
-const friends = document.getElementById("friends");
-const messages = document.getElementById("messages");
-const activity = document.getElementById("activity");
+const marker = document.getElementById('indicator');
+const items = document.querySelectorAll('.side-tab');
+const home = document.getElementById('home');
+const recent = document.getElementById('recent');
+const games = document.getElementById('games');
+const favs = document.getElementById('favs');
+const friends = document.getElementById('friends');
+const messages = document.getElementById('messages');
+const activity = document.getElementById('activity');
 
-window.onload = async function() {
-    await require('./js/launchers/find-games.js').loadGames();
-    document.querySelector('#loading-overlay').style.opacity = '0';
-    document.querySelector('#loading-overlay').style.visibility = 'hidden';
+window.onload = async function () {
+    // Timeout to prevent visual bugs at loading overlay
+    setTimeout(async () => {
+        await require('./js/launchers/find-games.js').loadGames();
+        document.querySelector('#loading-overlay').style.opacity = '0';
+        document.querySelector('#loading-overlay').style.visibility = 'hidden';
+    }, 200);
 };
 
-document.getElementById("home-btn").addEventListener("click", function () {
-    home.style.display = "flex";
-    recent.style.display = "none";
-    games.style.display = "none";
-    favs.style.display = "none";
-    friends.style.display = "none";
-    messages.style.display = "none";
-    activity.style.display = "none";
-})
+document.getElementById('home-btn').addEventListener('click', function () {
+    home.style.display = 'flex';
+    recent.style.display = 'none';
+    games.style.display = 'none';
+    favs.style.display = 'none';
+    friends.style.display = 'none';
+    messages.style.display = 'none';
+    activity.style.display = 'none';
+});
 
-document.getElementById("recent-btn").addEventListener("click", function () {
-    home.style.display = "none";
-    recent.style.display = "flex";
-    games.style.display = "none";
-    favs.style.display = "none";
-    friends.style.display = "none";
-    messages.style.display = "none";
-    activity.style.display = "none";
-})
+document.getElementById('recent-btn').addEventListener('click', function () {
+    home.style.display = 'none';
+    recent.style.display = 'flex';
+    games.style.display = 'none';
+    favs.style.display = 'none';
+    friends.style.display = 'none';
+    messages.style.display = 'none';
+    activity.style.display = 'none';
+});
 
-document.getElementById("games-btn").addEventListener("click", function () {
-    home.style.display = "none";
-    recent.style.display = "none";
-    games.style.display = "flex";
-    favs.style.display = "none";
-    friends.style.display = "none";
-    messages.style.display = "none";
-    activity.style.display = "none";
+document.getElementById('games-btn').addEventListener('click', function () {
+    home.style.display = 'none';
+    recent.style.display = 'none';
+    games.style.display = 'flex';
+    favs.style.display = 'none';
+    friends.style.display = 'none';
+    messages.style.display = 'none';
+    activity.style.display = 'none';
 
-    document.querySelector('div#games')
+    document.querySelector('div#games');
     require('./js/launchers/find-games.js').addGames();
-})
+});
 
-document.getElementById("favs-btn").addEventListener("click", function () {
-    home.style.display = "none";
-    recent.style.display = "none";
-    games.style.display = "none";
-    favs.style.display = "flex";
-    messages.style.display = "none";
-    activity.style.display = "none";
-    friends.style.display = "none";
-})
+document.getElementById('favs-btn').addEventListener('click', function () {
+    home.style.display = 'none';
+    recent.style.display = 'none';
+    games.style.display = 'none';
+    favs.style.display = 'flex';
+    messages.style.display = 'none';
+    activity.style.display = 'none';
+    friends.style.display = 'none';
+});
 
-document.getElementById("messages-btn").addEventListener("click", function () {
-    home.style.display = "none";
-    recent.style.display = "none";
-    games.style.display = "none";
-    favs.style.display = "none";
-    messages.style.display = "flex";
-    activity.style.display = "none";
-    friends.style.display = "none";
-})
+document.getElementById('messages-btn').addEventListener('click', function () {
+    home.style.display = 'none';
+    recent.style.display = 'none';
+    games.style.display = 'none';
+    favs.style.display = 'none';
+    messages.style.display = 'flex';
+    activity.style.display = 'none';
+    friends.style.display = 'none';
+});
 
-document.getElementById("activity-btn").addEventListener("click", function () {
-    home.style.display = "none";
-    recent.style.display = "none";
-    games.style.display = "none";
-    favs.style.display = "none";
-    messages.style.display = "none";
-    activity.style.display = "flex";
-    friends.style.display = "none";
-})
+document.getElementById('activity-btn').addEventListener('click', function () {
+    home.style.display = 'none';
+    recent.style.display = 'none';
+    games.style.display = 'none';
+    favs.style.display = 'none';
+    messages.style.display = 'none';
+    activity.style.display = 'flex';
+    friends.style.display = 'none';
+});
 
-document.getElementById("friends-btn").addEventListener("click", function () {
-    home.style.display = "none";
-    recent.style.display = "none";
-    games.style.display = "none";
-    favs.style.display = "none";
-    messages.style.display = "none";
-    activity.style.display = "none";
-    friends.style.display = "flex";
-})
+document.getElementById('friends-btn').addEventListener('click', function () {
+    home.style.display = 'none';
+    recent.style.display = 'none';
+    games.style.display = 'none';
+    favs.style.display = 'none';
+    messages.style.display = 'none';
+    activity.style.display = 'none';
+    friends.style.display = 'flex';
+});
 
-items.forEach(link => link.addEventListener("click", e => {
-    marker.style.top = "0";
-    marker.style.height = "0px";
-    indicator(e.target)
-}));
+items.forEach((link) =>
+    link.addEventListener('click', (e) => {
+        marker.style.top = '0';
+        marker.style.height = '0px';
+        indicator(e.target);
+    })
+);
 
 function indicator(item) {
-    marker.style.top = item.offsetTop + "px";
-    marker.style.height = "30px";
+    marker.style.top = item.offsetTop + 'px';
+    marker.style.height = '30px';
 }
 
 window.onkeydown = function (evt) {
-    if ((evt.code == "Minus" || evt.code == "Equal") && (evt.ctrlKey || evt.metaKey)) {
-        evt.preventDefault()
+    if ((evt.code == 'Minus' || evt.code == 'Equal') && (evt.ctrlKey || evt.metaKey)) {
+        evt.preventDefault();
     }
-}
+};
