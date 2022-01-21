4b52cd1015d6054e6721b3b7923005739b7fe3a1
LegItMate <skumar061975@gmail.com>
2022-01-21 13:38:24 +0530

    Search bar functionality

--- src/js/functions.js
+++ src/js/functions.js
@@ -13,6 +13,25 @@ function loadFile(event) {
 	});
 }
 
+const searchbar = document.querySelector('div.search-bar > input[type="text"]');
+// eslint-disable-next-line no-unused-vars
+searchbar.addEventListener('keyup', () => {
+	const query = searchbar.value;
+	const allGames = document.querySelectorAll('#gamesList > div[id^="game-div"]');
+
+	allGames.forEach((game) => {
+		if (game.id.split('-').slice(2).join('-').replaceAll('_', ' ').match(new RegExp(`${query}`, 'gi'))) {
+			game.style.display = 'block';
+		}
+		else if (query.length === 0) {
+			game.style.display = 'block';
+		}
+		else {
+			game.style.display = 'none';
+		}
+	});
+});
+
 document.getElementById('text').addEventListener('change', (e) => {
 	ipcRenderer.send('update-profile', {
 		username: e.target.value,
