75e141b41d7bae59d097c5e61ecc5211dc64069f
LegItMate <skumar061975@gmail.com>
2022-01-26 21:55:10 +0530

    Added Support for Minecraft Launcher(ms-version)

--- src/js/launchers/Minecraft.js
+++ src/js/launchers/Minecraft.js
@@ -6,7 +6,19 @@ async function getInstalledGames(os = process.platform) {
 	if (os === 'win32') {
 		const { stdout, error } = await exec('Reg query HKEY_CLASSES_ROOT\\Applications\\MinecraftLauncher.exe\\shell\\open\\command /ve').catch(() => { return { error: 'NOT_FOUND' }; });
 		if (error) {
-			return [];
+			const isInstalled = fs.existsSync('C:\\Program Files\\WindowsApps\\Microsoft.4297127D64EC6_1.0.113.0_x64__8wekyb3d8bbwe\\Minecraft.exe');
+			if (!isInstalled) return [];
+			const Location = 'C:\\Program Files\\WindowsApps\\Microsoft.4297127D64EC6_1.0.113.0_x64__8wekyb3d8bbwe';
+			const Executable = 'Minecraft.exe';
+			return [{
+				DisplayName: 'Minecraft Launcher',
+				LauncherName: 'Minecraft',
+				GameID: 'Minecraft',
+				Size: fs.statSync(Location),
+				Location,
+				Executable,
+				Args: [],
+			}];
 		}
 		else {
 			const Location = stdout.split('REG_SZ')[1].split('\r\n\r\n')[0].trim().split('" "')[0].split('"').join('').split('\\').slice(0, -1).join('\\');
@@ -15,7 +27,6 @@ async function getInstalledGames(os = process.platform) {
 			return [{
 				DisplayName: 'Minecraft Launcher',
 				LauncherName: 'Minecraft',
-				LaunchCommand: null,
 				GameID: 'Minecraft',
 				Size: fs.statSync(Location),
 				Location,
