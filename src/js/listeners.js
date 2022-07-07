bbb4df8c4fa74b125bebe63b248636e3a7346426
DashCruft <theporyafm12@gmail.com>
2022-07-06 19:39:53 -0700

    Check if DOMContentLoaded first

--- src/js/listeners.js
+++ src/js/listeners.js
@@ -14,7 +14,7 @@ const settingsbackblur = document.getElementById('settings-backblur');
 const { ipcRenderer } = require('electron');
 const fs = require('fs');
 
-window.onload = async function() {
+window.onload = async function () {
 	// Cache games first before loading
 	await require('./js/launchers/find-games.js').getInstalledGames();
 	document.getElementById('main-loading-overlay').style.opacity = '0';
@@ -22,153 +22,155 @@ window.onload = async function() {
 	setTimeout(() => require('./js/launchers/find-games.js').loadGames('recentGamesListMainPage'), 200);
 };
 
-document.querySelectorAll('.side-tab').forEach((link) =>
-	link.addEventListener('click', (e) => {
-		marker.style.top = '0';
-		marker.style.height = '0px';
-		indicator(e.target);
-	}),
-);
+window.addEventListener('DOMContentLoaded', (event) => {
+	document.querySelectorAll('.side-tab').forEach((link) =>
+		link.addEventListener('click', (e) => {
+			marker.style.top = '0';
+			marker.style.height = '0px';
+			indicator(e.target);
+		}),
+	);
 
-document.getElementById('alertboxexit').addEventListener('click', function() {
-	alertbox.style.display = 'none';
-});
-
-document.getElementById('home-btn').addEventListener('click', async function() {
-	home.style.display = 'flex';
-	recent.style.display = 'none';
-	games.style.display = 'none';
-	favs.style.display = 'none';
-	friends.style.display = 'none';
-	messages.style.display = 'none';
-	activity.style.display = 'none';
-
-	ipcRenderer.send('rpcUpdate', {
-		details: 'On Main Screen',
-		startTimestamp: Date.now(),
-		largeImageKey: 'lazap',
-		smallImageKey: 'home',
-		largeImageText: 'Lazap',
-		smallImageText: 'Home Screen',
+	document.getElementById('alertboxexit').addEventListener('click', function () {
+		alertbox.style.display = 'none';
 	});
 
-	await require('./js/launchers/find-games.js').loadGames('recentGamesListMainPage');
-});
+	document.getElementById('home-btn').addEventListener('click', async function () {
+		home.style.display = 'flex';
+		recent.style.display = 'none';
+		games.style.display = 'none';
+		favs.style.display = 'none';
+		friends.style.display = 'none';
+		messages.style.display = 'none';
+		activity.style.display = 'none';
 
-document.getElementById('recent-btn').addEventListener('click', async function() {
-	home.style.display = 'none';
-	recent.style.display = 'flex';
-	games.style.display = 'none';
-	favs.style.display = 'none';
-	friends.style.display = 'none';
-	messages.style.display = 'none';
-	activity.style.display = 'none';
+		ipcRenderer.send('rpcUpdate', {
+			details: 'On Main Screen',
+			startTimestamp: Date.now(),
+			largeImageKey: 'lazap',
+			smallImageKey: 'home',
+			largeImageText: 'Lazap',
+			smallImageText: 'Home Screen',
+		});
 
-	ipcRenderer.send('rpcUpdate', {
-		details: 'Browsing Recently Played',
-		startTimestamp: Date.now(),
-		largeImageKey: 'lazap',
-		smallImageKey: 'recent',
-		largeImageText: 'Lazap',
-		smallImageText: 'Recently Played',
+		await require('./js/launchers/find-games.js').loadGames('recentGamesListMainPage');
 	});
 
-	await require('./js/launchers/find-games.js').loadGames('recentGames');
-});
+	document.getElementById('recent-btn').addEventListener('click', async function () {
+		home.style.display = 'none';
+		recent.style.display = 'flex';
+		games.style.display = 'none';
+		favs.style.display = 'none';
+		friends.style.display = 'none';
+		messages.style.display = 'none';
+		activity.style.display = 'none';
 
-document.getElementById('games-btn').addEventListener('click', async function() {
-	home.style.display = 'none';
-	recent.style.display = 'none';
-	games.style.display = 'flex';
-	favs.style.display = 'none';
-	friends.style.display = 'none';
-	messages.style.display = 'none';
-	activity.style.display = 'none';
+		ipcRenderer.send('rpcUpdate', {
+			details: 'Browsing Recently Played',
+			startTimestamp: Date.now(),
+			largeImageKey: 'lazap',
+			smallImageKey: 'recent',
+			largeImageText: 'Lazap',
+			smallImageText: 'Recently Played',
+		});
 
-	ipcRenderer.send('rpcUpdate', {
-		details: 'Browsing All Games',
-		startTimestamp: Date.now(),
-		largeImageKey: 'lazap',
-		smallImageKey: 'games',
-		largeImageText: 'Lazap',
-		smallImageText: 'All Games',
+		await require('./js/launchers/find-games.js').loadGames('recentGames');
 	});
 
-	await require('./js/launchers/find-games.js').loadGames('allGames');
-});
+	document.getElementById('games-btn').addEventListener('click', async function () {
+		home.style.display = 'none';
+		recent.style.display = 'none';
+		games.style.display = 'flex';
+		favs.style.display = 'none';
+		friends.style.display = 'none';
+		messages.style.display = 'none';
+		activity.style.display = 'none';
 
-document.getElementById('favs-btn').addEventListener('click', async function() {
-	home.style.display = 'none';
-	recent.style.display = 'none';
-	games.style.display = 'none';
-	favs.style.display = 'flex';
-	messages.style.display = 'none';
-	activity.style.display = 'none';
-	friends.style.display = 'none';
+		ipcRenderer.send('rpcUpdate', {
+			details: 'Browsing All Games',
+			startTimestamp: Date.now(),
+			largeImageKey: 'lazap',
+			smallImageKey: 'games',
+			largeImageText: 'Lazap',
+			smallImageText: 'All Games',
+		});
 
-	ipcRenderer.send('rpcUpdate', {
-		details: 'Browsing Favourite Games',
-		startTimestamp: Date.now(),
-		largeImageKey: 'lazap',
-		smallImageKey: 'favs',
-		largeImageText: 'Lazap',
-		smallImageText: 'Favourites',
+		await require('./js/launchers/find-games.js').loadGames('allGames');
 	});
 
-	await require('./js/launchers/find-games.js').loadGames('favGames');
-});
+	document.getElementById('favs-btn').addEventListener('click', async function () {
+		home.style.display = 'none';
+		recent.style.display = 'none';
+		games.style.display = 'none';
+		favs.style.display = 'flex';
+		messages.style.display = 'none';
+		activity.style.display = 'none';
+		friends.style.display = 'none';
 
-document.getElementById('messages-btn').addEventListener('click', async function() {
-	home.style.display = 'none';
-	recent.style.display = 'none';
-	games.style.display = 'none';
-	favs.style.display = 'none';
-	messages.style.display = 'flex';
-	activity.style.display = 'none';
-	friends.style.display = 'none';
-});
+		ipcRenderer.send('rpcUpdate', {
+			details: 'Browsing Favourite Games',
+			startTimestamp: Date.now(),
+			largeImageKey: 'lazap',
+			smallImageKey: 'favs',
+			largeImageText: 'Lazap',
+			smallImageText: 'Favourites',
+		});
 
-document.getElementById('activity-btn').addEventListener('click', async function() {
-	home.style.display = 'none';
-	recent.style.display = 'none';
-	games.style.display = 'none';
-	favs.style.display = 'none';
-	messages.style.display = 'none';
-	activity.style.display = 'flex';
-	friends.style.display = 'none';
-});
+		await require('./js/launchers/find-games.js').loadGames('favGames');
+	});
 
-document.getElementById('friends-btn').addEventListener('click', async function() {
-	home.style.display = 'none';
-	recent.style.display = 'none';
-	games.style.display = 'none';
-	favs.style.display = 'none';
-	messages.style.display = 'none';
-	activity.style.display = 'none';
-	friends.style.display = 'flex';
-});
+	document.getElementById('messages-btn').addEventListener('click', async function () {
+		home.style.display = 'none';
+		recent.style.display = 'none';
+		games.style.display = 'none';
+		favs.style.display = 'none';
+		messages.style.display = 'flex';
+		activity.style.display = 'none';
+		friends.style.display = 'none';
+	});
 
-document.getElementById('settings-btn').addEventListener('click', async function() {
-	settingsbackblur.style.display = 'flex';
-	settings.style.display = 'flex';
-});
+	document.getElementById('activity-btn').addEventListener('click', async function () {
+		home.style.display = 'none';
+		recent.style.display = 'none';
+		games.style.display = 'none';
+		favs.style.display = 'none';
+		messages.style.display = 'none';
+		activity.style.display = 'flex';
+		friends.style.display = 'none';
+	});
 
-settingsbackblur.addEventListener('click', function() {
-	settings.style.display = 'none';
-	settingsbackblur.style.display = 'none';
-});
+	document.getElementById('friends-btn').addEventListener('click', async function () {
+		home.style.display = 'none';
+		recent.style.display = 'none';
+		games.style.display = 'none';
+		favs.style.display = 'none';
+		messages.style.display = 'none';
+		activity.style.display = 'none';
+		friends.style.display = 'flex';
+	});
 
-document.querySelector('.titlebar-settings').addEventListener('click', () => {
-	const Data = JSON.parse(fs.readFileSync('./storage/Settings/LauncherData.json').toString());
+	document.getElementById('settings-btn').addEventListener('click', async function () {
+		settingsbackblur.style.display = 'flex';
+		settings.style.display = 'flex';
+	});
+
+	settingsbackblur.addEventListener('click', function () {
+		settings.style.display = 'none';
+		settingsbackblur.style.display = 'none';
+	});
+
+	document.querySelector('.titlebar-settings').addEventListener('click', () => {
+		const Data = JSON.parse(fs.readFileSync('./storage/Settings/LauncherData.json').toString());
+		document.querySelectorAll('input[id^=setting-]').forEach((input) => {
+			input.checked = Data[input.id.split('-')[1]] ? true : false;
+		});
+	});
 	document.querySelectorAll('input[id^=setting-]').forEach((input) => {
-		input.checked = Data[input.id.split('-')[1]] ? true : false;
+		input.addEventListener('change', () => {
+			ipcRenderer.send('updateSetting', input.id.split('-')[1], document.querySelector(`input[id=${input.id}]`).checked);
+		});
 	});
-});
-document.querySelectorAll('input[id^=setting-]').forEach((input) => {
-	input.addEventListener('change', () => {
-		ipcRenderer.send('updateSetting', input.id.split('-')[1], document.querySelector(`input[id=${input.id}]`).checked);
-	});
-});
+})
 
 function indicator(item) {
 	marker.style.top = item.offsetTop + 'px';
