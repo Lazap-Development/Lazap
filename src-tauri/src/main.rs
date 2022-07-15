d68e0349d67daeb4ba3d53668e5a42c2d42fd45c
DashCruft <theporyafm12@gmail.com>
2022-07-14 17:10:06 -0700

    Added shadow & rounded corner support

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,15 @@
+use tauri::Manager;
+
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![run_game])
-        .run(tauri::generate_context!())
-        .expect("failed to run app");
+    .invoke_handler(tauri::generate_handler![run_game])
+    .setup(|app| {
+        let window = app.get_window(&"main").unwrap();
+        window_shadows::set_shadow(&window, true).expect("Unsupported platform!");
+        Ok(())
+    })
+    .run(tauri::generate_context!())
+    .expect("error while running tauri application");
 }
 
 #[tauri::command]
