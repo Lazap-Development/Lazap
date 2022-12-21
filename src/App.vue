4ade64e6905d2b230bbdbd90f20e6652a388914a
LegItMate <77503777+LegItMate@users.noreply.github.com>
2022-12-21 13:48:36 +0530

    feat(app): Add functionality of skipLogin

--- src/App.vue
+++ src/App.vue
@@ -18,10 +18,20 @@ export default {
 	},
 	async created() {
 		const os = window.__TAURI__.os;
+		const fs = window.__TAURI__.fs;
+		const path = window.__TAURI__.path;
 		console.info(`OS: ${await os.platform()} | Arch: ${await os.arch()}`);
 
 		if (!this.currentComponent) {
-			this.changeComponent('Main');
+			try {
+				let data = JSON.parse(await fs.readTextFile(await path.appDir() + 'storage/LauncherData.json', () => {
+					return;
+				}));
+				this.changeComponent(data?.skipLogin === true ? 'Main' : 'Login');
+			}
+			catch {
+				this.changeComponent('Login');
+			}
 		}
 	},
 	methods: {
