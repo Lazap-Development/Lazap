70205d75f099c0a6483ba0c57efaec63802ad448
DashCruft <theporyafm12@gmail.com>
2023-01-20 22:39:10 -0800

    v0.6.5

--- src/components/Settings.vue
+++ src/components/Settings.vue
@@ -93,7 +93,7 @@
         </div>
       </div>
 
-      <div class="settings-footer">v0.6.1 (Tauri Release)</div>
+      <div class="settings-footer">v0.6.5 (Tauri Release)</div>
     </div>
   </div>
 </template>
