63303961ce3b0a4ed59a713cb0760d50a974290f
DashCruft <theporyafm12@gmail.com>
2023-02-14 17:40:39 -0800

    Updated to version 0.6.3

--- src/components/Settings.vue
+++ src/components/Settings.vue
@@ -100,7 +100,7 @@
         </div>
       </div>
 
-      <div class="settings-footer">v0.6.2 (Tauri Release)</div>
+      <div class="settings-footer">v0.6.3 (Tauri Release)</div>
     </div>
   </div>
 </template>
