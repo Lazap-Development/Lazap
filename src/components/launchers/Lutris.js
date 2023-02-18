1226c99d4704e973d87783bc688a270b4a5abf81
DashCruft <theporyafm12@gmail.com>
2023-02-17 20:51:23 -0800

    Added error catch for lutris

--- src/components/launchers/Lutris.js
+++ src/components/launchers/Lutris.js
@@ -22,35 +22,39 @@ async function getInstalledGames(os = window.__TAURI__.os.platform()) {
   else if ((await os) === "linux") {
     let allDBGames = [];
     const dataDirPath = await window.__TAURI__.path.dataDir();
-    const lutris = await Database.load(`sqlite:${dataDirPath}/lutris/pga.db`);
-    const a = await lutris.select("SELECT * FROM games");
-    a.map((x) => {
-      const obj = {
-        DisplayName: x.name,
-        GameID: x.slug,
-        LauncherName: "Lutris",
-        LaunchID: x.id,
-        Executable: x.installer_slug,
-        Location: x.directory,
-        Size: x.installed_at,
-      };
-      allDBGames.push(obj);
-    });
-    /*await LutrisDB.all(`SELECT * FROM games`, (err, info) => {
-			info.forEach(x => {
-				const obj = {
-					DisplayName: x.name,
-					GameID: x.slug,
-					LauncherName: 'Lutris',
-					LaunchID: x.id,
-					Executable: x.installer_slug,
-					Location: x.directory,
-					Size: x.installed_at,
-				};
-				allDBGames.push(obj);
-			})
-		});*/
-    return allDBGames;
+    try {
+      const lutris = await Database.load(`sqlite:${dataDirPath}/lutris/pga.db`);
+      const a = await lutris.select("SELECT * FROM games");
+      a.map((x) => {
+        const obj = {
+          DisplayName: x.name,
+          GameID: x.slug,
+          LauncherName: "Lutris",
+          LaunchID: x.id,
+          Executable: x.installer_slug,
+          Location: x.directory,
+          Size: x.installed_at,
+        };
+        allDBGames.push(obj);
+      });
+      /*await LutrisDB.all(`SELECT * FROM games`, (err, info) => {
+        info.forEach(x => {
+          const obj = {
+            DisplayName: x.name,
+            GameID: x.slug,
+            LauncherName: 'Lutris',
+            LaunchID: x.id,
+            Executable: x.installer_slug,
+            Location: x.directory,
+            Size: x.installed_at,
+          };
+          allDBGames.push(obj);
+        })
+      });*/
+      return allDBGames;
+    } catch (err) {
+      return [];
+    }
   }
 }
 
