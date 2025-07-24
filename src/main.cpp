f8298c2f675e91b1f3804e644d39df6b405b08d9
p0ryae <contact@porya.me>
2025-07-24 12:18:25 -0700

    Small test

--- src/main.cpp
+++ src/main.cpp
@@ -8,65 +8,65 @@
 
 int main() {
 
-	// Setup window
-	glfwSetErrorCallback([](int error, const char* description) {
-		fprintf(stderr, "Glfw Error %d: %s\n", error, description);
-	});
-	glfwInit();
+  // Setup window
+  glfwSetErrorCallback([](int error, const char* description) {
+    fprintf(stderr, "Glfw Error %d: %s\n", error, description);
+  });
+  glfwInit();
 
-	const char* glsl_version = "#version 130";
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+  const char* glsl_version = "#version 130";
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
 
-	// Create window with graphics context
-	GLFWwindow* window = glfwCreateWindow(800, 600, "Lazap", nullptr, nullptr);
-	glfwMakeContextCurrent(window);
-	glfwSwapInterval(1); // vsync
+  // Create window with graphics context
+  GLFWwindow* window = glfwCreateWindow(800, 600, "Lazap", nullptr, nullptr);
+  glfwMakeContextCurrent(window);
+  glfwSwapInterval(1); // vsync
 
-	// Initialize OpenGL loader
-	glewInit();
+  // Initialize OpenGL loader
+  glewInit();
 
-	// Setup Dear ImGui context
-	IMGUI_CHECKVERSION();
-	ImGui::CreateContext();
+  // Setup Dear ImGui context
+  IMGUI_CHECKVERSION();
+  ImGui::CreateContext();
 
-	// Setup Dear ImGui style
-	ImGui::StyleColorsDark();
+  // Setup Dear ImGui style
+  ImGui::StyleColorsDark();
 
-	// Setup Platform/Renderer bindings
-	ImGui_ImplGlfw_InitForOpenGL(window, true);
-	ImGui_ImplOpenGL3_Init(glsl_version);
+  // Setup Platform/Renderer bindings
+  ImGui_ImplGlfw_InitForOpenGL(window, true);
+  ImGui_ImplOpenGL3_Init(glsl_version);
 
-	// Main loop
-	int display_w, display_h;
-	glfwGetFramebufferSize(window, &display_w, &display_h);
+  // Main loop
+  int display_w, display_h;
+  glfwGetFramebufferSize(window, &display_w, &display_h);
 
-	while (!glfwWindowShouldClose(window)) {
-		// Poll and handle events (inputs, window resize, etc.)
-		glfwPollEvents();
+  while (!glfwWindowShouldClose(window)) {
+    // Poll and handle events (inputs, window resize, etc.)
+    glfwPollEvents();
 
-		// Start the Dear ImGui frame
-		ImGui_ImplOpenGL3_NewFrame();
-		ImGui_ImplGlfw_NewFrame();
-		ImGui::NewFrame();
+    // Start the Dear ImGui frame
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui_ImplGlfw_NewFrame();
+    ImGui::NewFrame();
 
     ImGui::ShowDemoWindow();
     
-		// Rendering
-		ImGui::Render();
-		glfwGetFramebufferSize(window, &display_w, &display_h);
-		glViewport(0, 0, display_w, display_h);
-		glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
-		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-		glfwSwapBuffers(window);
-	}
+    // Rendering
+    ImGui::Render();
+    glfwGetFramebufferSize(window, &display_w, &display_h);
+    glViewport(0, 0, display_w, display_h);
+    glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+    glfwSwapBuffers(window);
+  }
 
-	// Cleanup
-	ImGui_ImplOpenGL3_Shutdown();
-	ImGui_ImplGlfw_Shutdown();
-	ImGui::DestroyContext();
+  // Cleanup
+  ImGui_ImplOpenGL3_Shutdown();
+  ImGui_ImplGlfw_Shutdown();
+  ImGui::DestroyContext();
 
-	glfwDestroyWindow(window);
-	glfwTerminate();
+  glfwDestroyWindow(window);
+  glfwTerminate();
 }
