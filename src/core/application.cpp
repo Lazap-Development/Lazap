5bf4fc8eaadc1ac8fde5ad05449362231886ca5d
p0ryae <contact@porya.me>
2025-12-05 11:01:19 -0800

    core: use core gl profile instead of compatibility

--- src/core/application.cpp
+++ src/core/application.cpp
@@ -77,8 +77,8 @@ void Application::run() {
   });
 
   glfwInit();
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
   glfwWindowHint(GLFW_DECORATED, GLFW_FALSE);
   glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
